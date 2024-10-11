a570970b783ef3d5d55808e6cb88c578907fb765
tatiana <taatt2000@gmail.com>
2024-10-11 09:52:59 +0200

    corrección

--- src/card/Card.jsx
+++ src/card/Card.jsx
@@ -1,12 +1,17 @@
 import styles from './card.module.css';
 
 console.log('styles')
+
 const Card = ({children, title, text, img}) => {
-return <div className={styles.card}>
+    const colorButton = styles[`button-${title}`]
+
+return <div className={`${styles.card} ${styles[title]}`}>
     <img src={img} alt="" />
     <h1 className={styles.title}>{children}</h1>
     <p className={styles.text}>{text}</p>
-    <button className={styles.button}>Learn More</button>
+    <button className={`${styles.button} ${colorButton}`}>Learn More</button>
 </div>
 }
 export default Card;
+
+// className={`${titlecolor} ${styles.color}`}
